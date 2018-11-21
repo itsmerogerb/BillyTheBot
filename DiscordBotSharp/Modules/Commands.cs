244a331869e54a355010461600494e79c85f4fe9
itsmerogerb <43283009+itsmerogerb@users.noreply.github.com>
2018-11-21 07:51:28 -0500

    Add files via upload

---
+++ DiscordBotSharp/Modules/Commands.cs
@@ -0,0 +1,91 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotSharp.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("purge", RunMode = RunMode.Async)]
+        [Alias("Purge")]
+        [Remarks("Purges An Amount Of Messages")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task Clear(int amountOfMessagesToDelete = 100000)
+        {
+
+            await (Context.Message.Channel as SocketTextChannel).DeleteMessagesAsync(await Context.Message.Channel.GetMessagesAsync(amountOfMessagesToDelete + 1).FlattenAsync());
+
+
+            if (amountOfMessagesToDelete == 100000)
+                await ReplyAsync("Cleared all messages");
+
+        }
+
+        [Command("!help")]
+        public async Task help()
+        {
+            IUser user = null;
+            user = user ?? Context.User;
+            await ReplyAsync("Hello " + user.Username + " here is what I can do for you \n" + "!ping = pong \n !poop = poop? " +
+                "\n !userInfo = get user info \n !multiply = mutiply two numbers \n !add = add two numbers");
+        }
+
+        [Command("Hi Billy")]
+        [Alias("hi billy", "Hi billy")]
+        public async Task PingAsync()
+        {
+
+            IUser user = null;
+            user = user ?? Context.User;
+            await ReplyAsync("Hello " + user.Username);
+        }
+
+        [Command("!ping")]
+        [Alias("pong", "hello")]
+        public Task PongAsync()
+            => ReplyAsync("pong!");
+
+
+        [Command("!poop")]
+        [Alias("!Poop", "!POOP")]
+        public async Task Poop()
+        {
+            await Clear(1);
+            await ReplyAsync(":poop:");
+        }
+      
+        [Command("!userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+        [Command("!multiply")]
+        [Summary("Get the product of two numbers.")]
+        public async Task Say(int a, int b)
+        {
+            int product = a * b;
+            await ReplyAsync($"The product of `{a} * {b}` is `{product}`.");
+        }
+
+        [Command("!add")]
+        [Summary("Get the product of two numbers.")]
+        public async Task Sa2y(int a, int b)
+        {
+            int product = a + b;
+            await ReplyAsync($"The product of `{a} + {b}` is `{product}`.");
+        }
+
+
+
+
+    }
+}
---
+++ DiscordBotSharp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+
+namespace DiscordBotSharp
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().BotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _Commands;
+        private IServiceProvider _Services;
+        public async Task BotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _Commands = new CommandService();
+            _Services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_Commands)
+            .BuildServiceProvider();
+
+            string botToken = "NTE0NDU3NDUwNTUyNzU0MTc4.DtW1nQ.x6dmt9MUCJIJe3Sf_rpxElbw8Dw";
+            // event subscibtion
+            _client.Log += Log;
+
+            await RegisterCommandsSync();
+            await _client.LoginAsync(Discord.TokenType.Bot, botToken);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+           
+           
+
+        }
+
+        private Task Log(LogMessage arg)
+        {
+
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsSync()
+        {
+            _client.MessageReceived += HandleCommandAsyc;
+            await _Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+
+
+        private async Task HandleCommandAsyc(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot) return;
+
+            int argPostion = 0;
+
+            if (!message.HasMentionPrefix(_client.CurrentUser, ref argPostion) || message.HasMentionPrefix(_client.CurrentUser, ref argPostion))
+            {
+                var context = new SocketCommandContext(_client, message);
+                var result = await _Commands.ExecuteAsync(context, argPostion, _Services);
+
+                if (result.IsSuccess)
+                    Console.WriteLine(message.Author.Username + " slapped Billy");
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
